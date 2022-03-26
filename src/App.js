dc66f26fcfbe9c6182a8d0d02e8f380940e57c90
Gustavo Ferreira da Silva <gusta23clash@gmail.com>
2022-03-26 20:57:32 -0300

    commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import Menu from "./components/Menu";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from "react-bootstrap";
+
+
+function App() {
+  return (
+    <div>
+      <Menu />
+      <h1>Hello World</h1>
+      <h1>Gustavo</h1>
+      <h1>Ferreira</h1>
+
+      <Button variant="outline-primary">Primary</Button>
+
+    </div>
+  );
+}
+
+export default App;
